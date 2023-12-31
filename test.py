974726395a8576c690615b11c356923902f373ac
Ariel <61569815+analyticsariel@users.noreply.github.com>
2023-12-31 11:53:13 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -6,13 +6,8 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 st.write(dir(conn))
 df = conn.read().iloc[:,:13]
 st.write(df.head())
-try:
-    st.write('spreadsheet', conn._spreadsheet)
-except:
-    st.write('spreadsheet', conn._spreadsheet())
-try:
-    st.write('client1', conn.client())
-except:
-    st.write('client2', conn.client())
-st.write('worksheet', conn._worksheet)
-st.write(conn.client().open('ml_training_prop_cond_dscr_20231228').sheet1)
+
+st.write('client1', conn.client)
+st.write(dir(conn.client))
+
+st.write(conn.client.open('ml_training_prop_cond_dscr_20231228').sheet1)
