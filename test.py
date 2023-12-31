19467c0f9bec96102ec4de173d8318b364aaa28e
Ariel <61569815+analyticsariel@users.noreply.github.com>
2023-12-31 11:45:46 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read().iloc[:,:13]
 st.write(df.head())
-st.write(conn._spreadsheet)
+st.write(conn.client.open('ml_training_prop_cond_dscr_20231228').sheet1)
 st.write(dir(conn))
