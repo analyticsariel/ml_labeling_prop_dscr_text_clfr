9d8901be1267f035ddd1e371d01f1478977f5235
Ariel <61569815+analyticsariel@users.noreply.github.com>
2023-12-31 10:57:05 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read()
+st.write(df.head())
