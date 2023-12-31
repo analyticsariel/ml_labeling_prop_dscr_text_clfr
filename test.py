34cc2dd94383a364be0d2e8a7bfef8c5c5933a69
Ariel <61569815+analyticsariel@users.noreply.github.com>
2023-12-31 11:46:37 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -3,8 +3,7 @@ from streamlit_gsheets import GSheetsConnection
 
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
-
+st.write(dir(conn))
 df = conn.read().iloc[:,:13]
 st.write(df.head())
 st.write(conn.client.open('ml_training_prop_cond_dscr_20231228').sheet1)
-st.write(dir(conn))
