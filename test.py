4724b64f5ba4a1aaace82e52a73c8931e0cc299f
Ariel <61569815+analyticsariel@users.noreply.github.com>
2023-12-31 11:50:48 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -10,5 +10,9 @@ try:
     st.write('spreadsheet', conn._spreadsheet)
 except:
     st.write('spreadsheet', conn._spreadsheet())
+try:
+    st.write('client1', conn.client)
+except:
+    st.write('client2', conn.client())
 st.write('worksheet', conn._worksheet)
 st.write(conn.client().open('ml_training_prop_cond_dscr_20231228').sheet1)
